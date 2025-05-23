1f2e1cbcbe752d1218fb9f4134e3c20fd08574e8
PolloMalo07 <santiagoara07@gmail.com>
2025-05-23 15:14:38 -0500

    Update ClaseEmpresa.py

--- ClaseEmpresa.py
+++ ClaseEmpresa.py
@@ -1,1 +1,22 @@
 ## Aqui ira todo lo resalcionado con todas las sedes que tenga la empresa. 
+from ClaseRestaurante import Restaurante
+
+class Empresa:
+    def __init__(self, nombre : str):
+
+        if len(nombre.strip()) == 0:
+            raise ValueError("la cadena con el nombre de la empresa no puede estar vacía")
+        
+        self.nombre = nombre.lower()
+        self.restaurantes = []
+
+    def agregar_restaurante(self, ciudad: str, dirreccion , horario , cantidad_de_mesas: int):
+        if len(ciudad.strip()) == 0 or len(dirreccion.strip()) == 0 or len(horario.strip()) == 0:
+            raise ValueError("La ciudad, dirección y horario no pueden estar vacíos")
+        
+        nuevo_restaurante = Restaurante(ciudad, dirreccion, horario, cantidad_de_mesas)
+        self.restaurantes.append(nuevo_restaurante)
+
+    def calcular_cantidad_Restaurantes(self):
+        return len(self.restaurantes)
+    
